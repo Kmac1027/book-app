8df7a8f2404740c565e985aec9d9e9d4f9a4ffac
Kevin McNeil <dogwoodrawks@gmail.com>
2020-09-22 17:15:56 -0700

    sara code change

--- server.js
+++ server.js
@@ -1,78 +1,81 @@
-"use strict";
+'use strict';
 
-//dependencies
-require("dotenv").config();
-const cors = require("cors");
-const PORT = process.env.PORT;
-const express = require("express");
+require('dotenv').config();
+require('ejs');
+
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+
 const app = express();
-app.use(cors());
-const superagent = require("superagent");
-let pg = require("pg");
+let port = process.env.PORT;
 const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', error => {
-  console.log(error);
-});
 
-// middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./public'));
 app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended : true}));
 
-// Routes
-app.get('/', homePage);
-app.get('/searches/new', renderSearchPage);
-app.post('/searches', searchHandle);
-app.get('*', errorHandler);
+app.get('/', renderHomePage);
+app.get('/search', renderSearchPage);
+app.post('/searches', getBookData);
+app.get(`*`, handleError);
 
-// Route functions
-function searchHandle(request, response) {
-  // console.log(request.body);
+function getBookData (request, response) {
   const searchQuery = request.body.search[0];
   const searchType = request.body.search[1];
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  if (searchType === 'title') { url += `+intitle:${searchQuery}` }
-  if (searchType === 'author') { url += `+inauthor:${searchQuery}` }
+  if(searchType === 'title'){ url += `+intitle:${searchQuery}`}
+  if(searchType === 'author'){ url += `+inauthor:${searchQuery}`}
   superagent.get(url)
-    .then(bookInfo => {
-      // console.log(bookInfo.body.items[0].volumeInfo.imageLinks);
-      const bookArray = bookInfo.body.items;
-      const finalBookArray = bookArray.map(book => new Book(book));
-      response.render('pages/searches/show.ejs', { finalBookArray: finalBookArray });
+    .then(data => {
+      const bookArray = data.body.items;
+      const finalBookArray = bookArray.map(book => new Book(book.volumeInfo));
+      response.render('pages/searches/show', {finalBookArray: finalBookArray});
+      let SQL = `INSERT INTO books (author, title, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5);`;
+      let safeValues = [finalBookArray[0].author, finalBookArray[0].title, finalBookArray[0].isbn, finalBookArray[0].url, finalBookArray[0].description];
+      client.query(SQL, safeValues)
+        .then(data => console.log(data + 'was stored'));
+  
     })
-    .catch((error) => {
-    console.error('error', error);
-    response.status(500).send('Unable to process request, please try again.');
-  });
+    .catch(() => {
+      response.status(500).send('Something went wrong with your location with your superagent location');
+      console.log(url);
+    })
+
 }
 
-function renderSearchPage(request, response) {
-  response.status(200).render('pages/searches/new.ejs');
+function renderHomePage(request, response) {
+  let sql = `SELECT * FROM books;`;
+  client.query(sql)
+    .then(result => {
+      let allBooks = result.rows;
+      console.log(allBooks);
+      response.status(200).render('pages/index.ejs', {bookList : allBooks});
+    })
 }
 
-function homePage(request, response) {
-  response.status(200).render('pages/index');
+function renderSearchPage(request,response) {
+  response.render('pages/searches/new.ejs');
 }
 
-// Constructor Functions
-function Book(book) {
-  this.title = book.volumeInfo.title ? book.volumeInfo.title : 'book not found';
-  this.author = book.volumeInfo.authors ? book.volumeInfo.authors : 'author not found';
-  this.description = book.volumeInfo.description;
-  this.image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : "https://i.imgur.com/J5LVHEL.jpg";
+function handleError (request, response) {
+  response.status(404).render('error');
+} 
+
+function Book(volumeInfo) {
+  this.url = volumeInfo.imageLinks ? volumeInfo.imageLinks.smallThumbnail.replace(/^http:\/\//i, 'https://'): `https://i.imgur.com/J5LVHEL.jpg`;
+  this.title = volumeInfo.title ? volumeInfo.title: ` Title Unavailable!`;
+  this.author = volumeInfo.authors ? volumeInfo.authors[0]: `Author Unavailable!`;
+  this.description = volumeInfo.description ? volumeInfo.description: `Description Not Found!?`;
+  this.isbn = volumeInfo.industryIdentifiers[0].identifier ? volumeInfo.industryIdentifiers[0].identifier: `No number available`;
 }
 
-function errorHandler(request, respond) {
-  respond.status(404).send('Unable to process request, please try again.');
-}
+
+
 
 client.connect()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`listening on ${PORT}`);
-    })
+    app.listen(port, () => {
+      console.log('Server is listening on port', port);
+    });
   })
-
-// app.listen(PORT, () => {
-//   console.log(`listening on ${PORT}`);
-// })
